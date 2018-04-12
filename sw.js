eeec5359a1b24ff0447a3318bda41323a9687eee
Tim Moreton <tmoreton89@gmail.com>
2018-04-11 22:57:29 -0400

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/casper/app-6704e792d071e5a06043.js","fe6c95897b481c6bc2dbdb91cf2faaee"],["/casper/app-a148af987e59242343b6.js","37b15017b8af0b5d08d8399fbd88d403"],["/casper/commons-2452f126e83e87bd8c65.js","b6655b893af02d9974a1d52bb5199522"],["/casper/commons-778953c02f99e29205bd.js","c4a89ccc3d041c7fd03fe2846ae23929"],["/casper/index.html","52b9a94a95e96f16a6b900c531161c08"],["/casper/manifest.json","580cdf75feea8ad18816993da45d222c"],["/casper/offline-plugin-app-shell-fallback/index.html","c4b777358ccd239857c5101e53310008"]];
+var precacheConfig = [["/casper/app-6704e792d071e5a06043.js","fe6c95897b481c6bc2dbdb91cf2faaee"],["/casper/commons-778953c02f99e29205bd.js","c4a89ccc3d041c7fd03fe2846ae23929"],["/casper/index.html","01d592bd192e633c0db89ebce271afba"],["/casper/manifest.json","580cdf75feea8ad18816993da45d222c"],["/casper/offline-plugin-app-shell-fallback/index.html","bea6e2796e1289b289426e979e770ffd"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
